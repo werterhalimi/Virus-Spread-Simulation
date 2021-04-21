c35c34c3550519acdab928b13dfbbac29e485ef3
Werter <shai.halimi@gmail.com>
2021-04-21 15:53:12 +0200

    Fiexed paint and human moves

--- java/src/main/java/ch/shaihalimi/gui/Simulation.java
+++ java/src/main/java/ch/shaihalimi/gui/Simulation.java
@@ -1,6 +1,7 @@
 package ch.shaihalimi.gui;
 
 import ch.shaihalimi.Main;
+import ch.shaihalimi.human.Direction;
 import ch.shaihalimi.human.Human;
 import ch.shaihalimi.human.Staff;
 import ch.shaihalimi.human.Student;
@@ -13,6 +14,7 @@ public class Simulation extends JFrame {
     private Configuration config;
     private Main instance;
     private Human[] humans;
+    private Graphics gr;
     private boolean running;
 
     public Simulation(Main main,Configuration config)  {
@@ -24,16 +26,24 @@ public class Simulation extends JFrame {
         this.getContentPane().setBackground(Color.BLACK);
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.init();
-        this.start();
+        //this.start();
     }
 
     @Override
     public void paint(Graphics g) {
         super.paintComponents(g);
+        g.clearRect(0,0,1000,1000);
         Graphics2D g2d = (Graphics2D) g;
-        System.out.println("painting");
-        for (Human human : this.humans)
+        for (Human human : this.humans){
             human.draw(g2d);
+            human.move(Direction.values()[this.getInstance().getRandom().nextInt(7)],this.getInstance().getRandom().nextInt(500));
+        }
+        try {
+            Thread.sleep(30);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        this.paint(g);
     }
 
     public void init(){
@@ -43,16 +53,19 @@ public class Simulation extends JFrame {
         for (int i = this.config.getStudent(); i < this.config.getHumansNumber(); i++)
             this.humans[i] = new Staff(this);
         humans[58].setHealthy(false);
-
+        Human h = this.humans[58];
+        this.humans[50].move(h.getX(),h.getY()+150);
     }
 
     public void start()  {
         this.running = true;
+        int i =0;
         while(isRunning()){
             for (Human human : humans) {
                 human.move(human.getX() + this.getInstance().randomBetween(1, -1), human.getY() + this.getInstance().randomBetween(1, -1));
             }
-            this.repaint();
+            SwingUtilities.updateComponentTreeUI(this);
+            System.out.println(i++);
             try {
                 Thread.currentThread().sleep(1000/30);
             } catch (InterruptedException e) {
